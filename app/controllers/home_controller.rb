467d533a8e7722680325b12a13422074c80f4e50
Vaizz <akiba788@mail.ru>
2015-03-03 21:09:47 +0300

    get info from time.jsontest.com

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,14 @@
 class HomeController < ApplicationController
 
+  require 'open-uri'
+  
   def index
+  end
 
+  def show
+    source = 'http://time.jsontest.com/'
+    @value = JSON.load ( open (source) )
+    render json: @value
   end
 
 end
